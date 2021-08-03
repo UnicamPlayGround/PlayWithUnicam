b3906a31fa4c529b87212fefc1203651b914b28e
matteoRondyUni <56674402+matteoRondyUni@users.noreply.github.com>
2021-08-03 16:03:30 +0200

    Updates

---
+++ backend/multiplayer/game.js
@@ -0,0 +1,17 @@
+const db = require('../database');
+
+//TODO commentare
+exports.getListaGiochi = (cb) => {
+    return db.pool.query('select nome, tipo, max_giocatori, min_giocatori, link from public.giochi where attivo=$1',
+        [true], (error, results) => {
+            cb(error, results)
+        });
+}
+
+//TODO commentare
+exports.lancioDado = (nFacce) => {
+    if (nFacce == 0 || nFacce == null || isNaN(parseInt(nFacce)))
+        throw "Errore, il numero delle facce del dado deve essere un numero maggiore di 0!";
+
+    return Math.floor(Math.random() * nFacce) + 1;
+}
