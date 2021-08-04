0ea76f31fe94e87b4d85be52153db519dd51b1e9
matteoRondyUni <56674402+matteoRondyUni@users.noreply.github.com>
2021-08-04 16:15:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -129,33 +129,58 @@ app.post('/login/utente', (req, res) => {
             const user = JSON.parse(JSON.stringify(results.rows));
 
             const toControl = bcrypt.hashSync(req.body.password + SECRET_PWD, user[0].salt);
-            const result = (user[0].password == toControl);
-            if (!result) return res.status(401).send('Password non valida!');
+            if (!(user[0].password == toControl)) return res.status(401).send('Password non valida!');
 
-            const toSend = { username: user[0].username, tipo: user[0].tipo };
-            sendAccessToken(res, toSend);
+            sendAccessToken(res, { username: user[0].username, tipo: user[0].tipo });
         })
     } catch (error) {
         return res.status(400).send(error);
     }
 });
 
+//TODO
+app.post('/login/ospiti', (req, res) => {
+    try {
+        utente.cercaUtenteByUsername(req.body.username, (err, results) => {
+            if (err) return res.status(500).send('Server Error!');
+            if (!controller.controllaRisultatoQuery(results)) return res.status(404).send("L'username " + req.body.username + " è già in uso!");
+
+            utente.cercaOspiteByUsername(req.body.username, (err, results) => {
+                if (err) return res.status(500).send('Server Error!');
+                if (!controller.controllaRisultatoQuery(results)) return res.status(404).send("L'username " + req.body.username + " è già in uso!");
+
+                utente.creaOspite(req.body.username, (err, results) => {
+                    if (err) return res.status(400).send("NON E' STATO POSSIBILE CREARE L'OSPITE!");
+                    sendAccessToken(res, { username: req.body.username, tipo: "OSPITE" });
+                });
+            })
+        })
+    } catch (error) {
+        return res.status(400).send(error);
+    }
+})
+
 /**
  * REST - Registrazione
  */
 app.post('/register/utente', (req, res) => {
     try {
-        utente.cercaUtenteByUsername(req.body.username, (err, results) => {
-            try {
-                if (err) return res.status(500).send('Server error!');
-                const users = JSON.parse(JSON.stringify(results.rows));
+        utente.cercaOspiteByUsername(req.body.username, (err, results) => {
+            if (err) return res.status(500).send('Server Error!');
+            if (!controller.controllaRisultatoQuery(results)) return res.status(404).send("L'username " + req.body.username + " è già in uso!");
 
-                if (users.length == 0)
-                    utente.creaUtente(req.body, res);
-                else return res.status(400).send("L'username \'" + users[0].username + "\' è già stato usato!");
-            } catch (error) {
-                return res.status(400).send(error);
-            }
+            utente.cercaUtenteByUsername(req.body.username, (err, results) => {
+                try {
+                    if (err) return res.status(500).send('Server error!');
+                    const users = JSON.parse(JSON.stringify(results.rows));
+
+                    if (users.length == 0)
+                        utente.creaUtente(req.body.username, req.body.password, res);
+                    else return res.status(400).send("L'username \'" + users[0].username + "\' è già stato usato!");
+                } catch (error) {
+                    return res.status(400).send(error);
+                }
+            });
         });
     } catch (error) {
         return res.status(400).send(error);
@@ -170,32 +195,6 @@ app.post('/lobby', (req, res) => {
         return res.status(400).send(error);
     }
 })
-
-//TODO
-app.post('/login/ospiti', (req, res) => {
-    console.log(req.body.username);
-    try {
-        utente.cercaUtenteByUsername(req.body.username, (err, results) => {
-            
-            if (err) return res.status(500).send('Server Error!');
-            if (!controller.controllaRisultatoQuery(results)) return res.status(404).send("L'username " + req.body.username + " è già in uso");
-
-            utente.cercaOspiteByUsername(req.body.username, (err, results) => {
-                if (err) return res.status(500).send('Server Error!');
-                if (!controller.controllaRisultatoQuery(results)) return res.status(404).send("L'username " + req.body.username + " è già in uso");
-
-                utente.creaOspite(req.body.username, (err, results) => {
-                    if (err) return res.status(400).send("NON E' STATO POSSIBILE CREARE L'OSPITE!");
-                    const toSend = { username: req.body.username, tipo: "OSPITE" };
-                    sendAccessToken(res, toSend);
-                });
-            })
-        })
-    } catch (error) {
-        return res.status(400).send(error);
-    }
-})
-
 
 app.get('/*', function (req, res) {
     res.sendFile('index.html', { root: __dirname + '/www' });
