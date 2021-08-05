755f5f6e4807c43fe375fbe6aa97429f0cdcdeee
Tommaso Catervi <56674392+catervpillar@users.noreply.github.com>
2021-08-05 16:41:58 +0200

    update frontend lobby

--- src/app/lobby/lobby-home/lobby-home.page.ts
+++ src/app/lobby/lobby-home/lobby-home.page.ts
@@ -7,16 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LobbyHomePage implements OnInit {
   segment: string = "impostazioni";
+  guests = [];
+  gioco = { nome: 'Gioco 1', min_giocatori: 2, max_giocatori: 6, img: 'https://bit.ly/376IQJU' };
+  pubblica = false;
+  codice;
 
 
-  constructor() { }
+  ospiti = [
+    { username: 'cipo' },
+    { username: 'rondy' },
+    { username: 'tommi' },
+    { username: 'cla' },
+    { username: 'gre' },
+    { username: 'ludo' },
+    { username: 'pippo' },
+    { username: 'pluto' }
+  ]
 
-  ngOnInit() {
-    
+  constructor() {
+    this.loadinfo();
+    this.loadGuests();
   }
 
-  segmentChanged(ev: any){
+  ngOnInit() {
+
+  }
+
+  segmentChanged(ev: any) {
     this.segment = ev.detail.value;
+  }
+
+  loadinfo() {
+
+  }
+
+  loadGuests() {
+    //TODO: chiamata REST
+    this.guests = this.guests.concat(this.ospiti);
+  }
+
+  salvaModifiche() { 
+    
   }
 
 }
