6c9e9cbd94fe8da169217a66b6d68b27f526c3f7
matteoRondyUni <56674402+matteoRondyUni@users.noreply.github.com>
2021-08-30 15:12:50 +0200

    Aggiunta Commenti

--- backend/utente.js
+++ backend/utente.js
@@ -4,88 +4,15 @@ const bcrypt = require('bcrypt');
 
 //TODO
 const SECRET_PWD = "secret";
-const SECRET_KEY = "secret_jwt";
 
-//TODO da commentare
-exports.cercaUtenteByUsername = (username, cb) => {
-    controller.controllaNotNull(username, "L'username non deve essere vuoto!");
-    return db.pool.query('SELECT * FROM public.utenti WHERE username = $1', [username], (error, results) => {
-        cb(error, results)
-    });
-}
-
-//TODO da commentare
-exports.cercaOspiteByUsername = (username, cb) => {
-    controller.controllaNotNull(username, "L'username non deve essere vuoto!");
-    return db.pool.query('SELECT * FROM public.ospiti WHERE username = $1', [username], (error, results) => {
-        cb(error, results)
-    });
-}
-
-//TODO
-exports.creaUtente = (username, nome, cognome, password, response) => {
-    //TODO: metterli tutti in un unico metodo
-    controller.controllaString(username, "Il campo 'Username' non deve essere vuoto!");
-    controller.controllaString(nome, "Il campo 'Nome' non deve essere vuoto!");
-    controller.controllaString(cognome, "Il campo 'Cognome' non deve essere vuoto!");
-    controller.controllaPassword(password);
-
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password + SECRET_PWD, salt);
-
-    db.pool.query('INSERT INTO public.utenti (username, nome, cognome, password, salt, tipo) VALUES ($1, $2, $3, $4, $5, $6)',
-        [username, nome, cognome, hash, salt, "GIOCATORE"], (error, results) => {
-            if (error) return response.status(400).send("NON E' STATO POSSIBILE CREARE L'UTENTE!");
-            return response.status(200).send({ 'esito': "1" });
-        })
-}
-
-//TODO
-exports.creaOspite = (username, cb) => {
-    controller.controllaNotNull(username, "L'username non deve essere vuoto!");
-
-    db.pool.query('INSERT INTO public.ospiti (username) VALUES ($1)',
-        [username], (error, results) => {
-            cb(error, results)
-        })
-}
-
-//TODO
-exports.modificaNomeCognome = (username, nome, cognome, response) => {
-    controller.controllaString(nome, "Il nome non è valido!");
-    controller.controllaString(cognome, "il cognome non è valido!");
-
-    this.cercaUtenteByUsername(username, (err, results) => {
-        if (err) return response.status(500).send('Server Error!');
-        if (controller.controllaRisultatoQuery(results)) return response.status(404).send('Utente non trovato!');
-        db.pool.query('UPDATE public.utenti SET nome = $1, cognome = $2 WHERE username = $3',
-            [nome, cognome, username], (error, results) => {
-                if (error) {
-                    console.log(error);
-                    return response.status(400).send('Errore dati query');
-                }
-                return response.status(200).send({ 'esito': "1" });
-            })
-    });
-}
-
-exports.modificaUsername = (oldUsername, newUsername, response, cb) => {
-    controller.controllaString(newUsername, "Il nuovo username non è valido!");
-
-    this.cercaUtenteByUsername(oldUsername, (err, results) => {
-        if (err) return response.status(500).send('Server Error!');
-        if (controller.controllaRisultatoQuery(results)) return response.status(404).send('Utente non trovato!');
-
-        this.cercaUtenteByUsername(newUsername, (err, results) => {
-            if (!controller.controllaRisultatoQuery(results)) return response.status(404).send("Il nuovo username è già utilizzato!");
-            db.pool.query('UPDATE public.utenti SET username = $1 WHERE username = $2',
-                [newUsername, oldUsername], (error, results) => {
-                    cb(error, results);
-                })
-        })
-    });
-}
-
+/**
+ * Modifica la Password di un Utente.
+ * @param {string} newPassword Nuova Password
+ * @param {string} oldPassword Vecchia Password
+ * @param {*} response 
+ * @param {string} username Username dell'Utente che deve modificare la password
+ * @returns L'esito della modifica
+ */
 exports.cambiaPassword = (newPassword, oldPassword, response, username) => {
     try {
         controller.controllaPassword(newPassword);
@@ -115,9 +42,129 @@ exports.cambiaPassword = (newPassword, oldPassword, response, username) => {
     });
 }
 
+/**
+ * Ricerca un Ospite tramite il suo Username.
+ * @param {string} username Username dell'Ospite da ricercare
+ * @param {*} cb Callback
+ * @returns Ritorna le informazioni dell'Ospite
+ */
+exports.cercaOspiteByUsername = (username, cb) => {
+    controller.controllaNotNull(username, "L'username non deve essere vuoto!");
+    return db.pool.query('SELECT * FROM public.ospiti WHERE username = $1', [username], (error, results) => {
+        cb(error, results)
+    });
+}
+
+/**
+ * Ricerca un Utente tramite il suo Username.
+ * @param {string} username Username dell'Utente da ricercare
+ * @param {*} cb Callback
+ * @returns Ritorna le informazioni dell'Utente
+ */
+exports.cercaUtenteByUsername = (username, cb) => {
+    controller.controllaNotNull(username, "L'username non deve essere vuoto!");
+    return db.pool.query('SELECT * FROM public.utenti WHERE username = $1', [username], (error, results) => {
+        cb(error, results)
+    });
+}
+
+/**
+ * Crea un nuovo Ospite.
+ * @param {string} username Username dell'Ospite da creare
+ * @param {*} cb Callback
+ */
+exports.creaOspite = (username, cb) => {
+    controller.controllaNotNull(username, "L'username non deve essere vuoto!");
+
+    db.pool.query('INSERT INTO public.ospiti (username) VALUES ($1)',
+        [username], (error, results) => {
+            cb(error, results)
+        })
+}
+
+/**
+ * Crea un nuovo Utente.
+ * @param {string} username Username dell'Utente da creare
+ * @param {string} nome Nome dell'Utente da creare
+ * @param {string} cognome Cognome dell'Utente da creare
+ * @param {string} password Password dell'Utente da creare
+ * @param {*} response 
+ */
+exports.creaUtente = (username, nome, cognome, password, response) => {
+    //TODO: metterli tutti in un unico metodo
+    controller.controllaString(username, "Il campo 'Username' non deve essere vuoto!");
+    controller.controllaString(nome, "Il campo 'Nome' non deve essere vuoto!");
+    controller.controllaString(cognome, "Il campo 'Cognome' non deve essere vuoto!");
+    controller.controllaPassword(password);
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password + SECRET_PWD, salt);
+
+    db.pool.query('INSERT INTO public.utenti (username, nome, cognome, password, salt, tipo) VALUES ($1, $2, $3, $4, $5, $6)',
+        [username, nome, cognome, hash, salt, "GIOCATORE"], (error, results) => {
+            if (error) return response.status(400).send("NON E' STATO POSSIBILE CREARE L'UTENTE!");
+            return response.status(200).send({ 'esito': "1" });
+        })
+}
+
+/**
+ * Ritorna l'Username, il Nome ed il Cognome di un Utente.
+ * @param {string} username Username dell'Utente
+ * @param {*} cb Callback
+ * @returns Ritorna le informazioni dell'Utente
+ */
 exports.getUserInfo = (username, cb) => {
     return db.pool.query('select username, nome, cognome from public.utenti where username=$1',
         [username], (error, results) => {
             cb(error, results)
         });
+}
+
+/**
+ * Modifica il Nome ed il Cognome di un Utente.
+ * @param {string} username Username dell'Utente da modificare
+ * @param {string} nome Nuovo Nome da impostare
+ * @param {string} cognome Nuovo Cognome da impostare
+ * @param {*} response 
+ */
+exports.modificaNomeCognome = (username, nome, cognome, response) => {
+    controller.controllaString(nome, "Il campo 'Nome' non è valido!");
+    controller.controllaString(cognome, "Il campo 'Cognome' non è valido!");
+
+    this.cercaUtenteByUsername(username, (err, results) => {
+        if (err) return response.status(500).send('Server Error!');
+        if (controller.controllaRisultatoQuery(results)) return response.status(404).send('Utente non trovato!');
+        db.pool.query('UPDATE public.utenti SET nome = $1, cognome = $2 WHERE username = $3',
+            [nome, cognome, username], (error, results) => {
+                if (error) {
+                    console.log(error);
+                    return response.status(400).send('Errore dati query');
+                }
+                return response.status(200).send({ 'esito': "1" });
+            })
+    });
+}
+
+/**
+ * Modifica l'Username di un Utente.
+ * @param {string} oldUsername Vecchio Username
+ * @param {string} newUsername Nuovo Username
+ * @param {*} response 
+ * @param {*} cb Callback
+ */
+exports.modificaUsername = (oldUsername, newUsername, response, cb) => {
+    controller.controllaString(newUsername, "Il nuovo username non è valido!");
+
+    this.cercaUtenteByUsername(oldUsername, (err, results) => {
+        if (err) return response.status(500).send('Server Error!');
+        if (controller.controllaRisultatoQuery(results)) return response.status(404).send('Utente non trovato!');
+
+        this.cercaUtenteByUsername(newUsername, (err, results) => {
+            if (!controller.controllaRisultatoQuery(results)) return response.status(404).send("Il nuovo username è già utilizzato!");
+            db.pool.query('UPDATE public.utenti SET username = $1 WHERE username = $2',
+                [newUsername, oldUsername], (error, results) => {
+                    cb(error, results);
+                })
+        })
+    });
 }
