26c15919a6c0540565d9cc70b7b01338f531b8c6
matteoRondyUni <56674402+matteoRondyUni@users.noreply.github.com>
2021-09-02 16:39:01 +0200

    Update alert-creator.service.ts
    Co-Authored-By: C1PO <74489318+C1PO@users.noreply.github.com>
    Co-Authored-By: Tommaso Catervi <56674392+catervpillar@users.noreply.github.com>

--- src/app/services/alert-creator/alert-creator.service.ts
+++ src/app/services/alert-creator/alert-creator.service.ts
@@ -9,31 +9,23 @@ export class AlertCreatorService {
   constructor(private alertController: AlertController) { }
 
   async createConfirmationAlert(messaggio, cb) {
+    var buttons = [
+      { text: 'ANNULLA', role: 'cancel' },
+      { text: 'OK', handler: () => { cb(); } }
+    ]
+    this.createAlert("Conferma", messaggio, buttons);
+  }
+
+  async createAlert(header, messaggio, buttons) {
     const alert = await this.alertController.create({
-      header: 'Conferma',
+      header: header,
       message: messaggio,
-      buttons: [
-        {
-          text: 'ANNULLA',
-          role: 'cancel',
-        },
-        {
-          text: 'OK',
-          handler: () => {
-            cb();
-          }
-        }
-      ]
+      buttons: buttons
     });
     await alert.present();
   }
 
-  async createInfoAlert(header, messaggio){
-    const alert = await this.alertController.create({
-      header: header,
-      message: messaggio,
-      buttons: ['OK'],
-    });
-    await alert.present();
+  async createInfoAlert(header, messaggio) {
+    this.createAlert(header, messaggio, ['OK']);
   }
 }
