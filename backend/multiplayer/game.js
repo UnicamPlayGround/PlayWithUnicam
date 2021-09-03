636149305d144153b8d012fd476ef793a1756d6d
Tommaso Catervi <56674392+catervpillar@users.noreply.github.com>
2021-09-03 18:13:04 +0200

    aggiunta chiamata REST per ottenere il file config di gioco

--- backend/multiplayer/game.js
+++ backend/multiplayer/game.js
@@ -1,4 +1,6 @@
 const db = require('../database');
+const lobby = require('./lobby');
+const controller = require('../controller');
 
 //TODO commentare
 exports.getListaGiochi = (cb) => {
@@ -22,4 +24,19 @@ exports.getInfoGioco = (idGioco, cb) => {
         [idGioco], (error, results) => {
             cb(error, results)
         });
+}
+
+//TODO commentare
+exports.getConfigGioco = (username, cb) => {
+    lobby.cercaLobbyByUsername(username, (error, results) => {
+        if (error) return response.status(400).send("Non è stato possibile trovare la Lobby");
+        if (controller.controllaRisultatoQuery(results))
+            return response.status(400).send("Errore: Devi partecipare ad una Lobby!");
+
+        const lobby = JSON.parse(JSON.stringify(results.rows))[0];
+
+        db.pool.query('select config from public.giochi where id=$1', [lobby.id_gioco], (error, results) => {
+            cb(error, results)
+        });
+    })
 }
